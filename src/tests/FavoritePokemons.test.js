0fe495da3833908563f136191f8d105b2b792290
esmeraldo17 <esmeraldoalberto55@gmail.com>
2022-09-22 01:20:21 +0100

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+test('Teste se é exibida na tela a mensagem No favorite pokemon found', () => {
+  renderWithRouter(<FavoritePokemons />);
+
+  const p = screen.getByText(/no favorite pokemon found/i);
+
+  expect(p).toBeInTheDocument();
+});
