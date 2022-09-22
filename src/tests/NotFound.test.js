907448ea83f60a848e2a3ad405f14f98b8f797ea
esmeraldo17 <esmeraldoalberto55@gmail.com>
2022-09-22 01:34:29 +0100

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+test('Teste se a página contém um heading h2 c ', () => {
+  renderWithRouter(<NotFound />);
+
+  const headingTwo = screen.getByRole(
+    'heading',
+    { level: 2, name: /Page requested not found/i },
+  );
+
+  expect(headingTwo).toBeInTheDocument();
+});
+
+test('Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+  renderWithRouter(<NotFound />);
+
+  const img = screen.getByRole('img');
+
+  expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
